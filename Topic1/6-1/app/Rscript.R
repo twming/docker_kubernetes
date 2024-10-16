f3ce92c6c73e11eb8a1838143e8eda2f082eb699
twming <tanwoeiming@gmail.com>
2024-10-16 23:15:55 +0800

    Add files via upload

---
+++ Topic1/6-1/app/Rscript.R
@@ -0,0 +1,7 @@
+install.packages('ggplot2')
+require(ggplot2)
+data=read.csv("olympics.csv")
+data_gold=data[data$Medal=='Gold',]
+data_gold$Participants=1
+data_sum=aggregate(Participants~Edition+Gender, data_gold, sum)
+ggplot(data_sum, aes(Edition, Participants,colour=Gender)) + geom_line() + geom_point()
